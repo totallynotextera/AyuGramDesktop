3b32de77c98399f8dc1a9aa316dbcec0b7602539
ZavaruKitsu <alexeyzavar@gmail.com>
2023-05-24 14:30:18 +0300

    feat: implement sendUploadProgress

--- Telegram/SourceFiles/api/api_send_progress.cpp
+++ Telegram/SourceFiles/api/api_send_progress.cpp
@@ -15,6 +15,12 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "data/data_peer_values.h"
 #include "apiwrap.h"
 
+
+// AyuGram includes
+#include "core/application.h"
+#include "core/core_settings.h"
+
+
 namespace Api {
 namespace {
 
@@ -112,6 +118,14 @@ void SendProgressManager::send(const Key &key, int progress) {
 	if (skipRequest(key)) {
 		return;
 	}
+
+    // AyuGram sendUploadProgress
+    const auto settings = &Core::App().settings();
+    if (!settings->sendUploadProgress()) {
+        DEBUG_LOG(("[AyuGram] Don't send upload progress"));
+        return;
+    }
+
 	using Type = SendProgressType;
 	const auto action = [&]() -> MTPsendMessageAction {
 		const auto p = MTP_int(progress);
